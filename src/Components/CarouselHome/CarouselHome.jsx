ff2d2f505f4f92aebb5019b23fcd3d916866ac34
Mauricio <mauriruffini32@gmail.com>
2024-06-24 16:32:51 -0300

    date color botones

--- src/Components/CarouselHome/CarouselHome.jsx
+++ src/Components/CarouselHome/CarouselHome.jsx
@@ -3,14 +3,13 @@ import Carousel from "react-bootstrap/Carousel";
 import image1 from "../../imagenes/image1.gif";
 import image2 from "../../imagenes/image2.jpg";
 import image3 from "../../imagenes/image3.jpg";
-import { Container } from "react-bootstrap";
 
 function CarouselHome() {
   return (
     
-      <Carousel className="w-5">
+      <Carousel className="w-5 ">
         <Carousel.Item interval={5000}>
-          <img className="w-100 " src={image1} alt="img1" />
+          <img className="w-100" src={image1} alt="img1" />
         </Carousel.Item>
         <Carousel.Item interval={5000}>
           <img className="w-100"  src={image2} alt="img2" />
