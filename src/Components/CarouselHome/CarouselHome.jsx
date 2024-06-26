ecd36c0201c07933d2e6b7148f59931a47c887f1
Mauricio <mauriruffini32@gmail.com>
2024-06-26 15:28:58 -0300

    carousel

--- src/Components/CarouselHome/CarouselHome.jsx
+++ src/Components/CarouselHome/CarouselHome.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import image1 from "../../imagenes/image1.gif";
-import image2 from "../../imagenes/image2.jpg";
-import image3 from "../../imagenes/image3.jpg";
+import image1 from "../../imagenes/1. CONOCE NUESTROS SERVICIOS.gif";
+import image2 from "../../imagenes/2. PROMOS.png";
+import image3 from "../../imagenes/3. ENVIOS.png";
+import "./CarouselHome.css"
 
 function CarouselHome() {
   return (
     
       <Carousel className="w-5 ">
         <Carousel.Item interval={5000}>
-          <img className="w-100" src={image1} alt="img1" />
+          <img className="w-100 " src={image1} alt="img1" />
         </Carousel.Item>
         <Carousel.Item interval={5000}>
           <img className="w-100"  src={image2} alt="img2" />
